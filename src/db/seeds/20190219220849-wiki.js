d320400ee0ff83822b8c392e96e16b8e5c37bddb
Cesar Jimenez <cijgr@hotmail.com>
2019-02-19 17:25:33 -0600

    Checkpoint 7 seeding data: completed

---
+++ src/db/seeds/20190219220849-wiki.js
@@ -0,0 +1,29 @@
+'use strict';
+const faker = require("faker");
+const User = require("../models").User;
+
+let wikis = [];
+
+for (let i = 1; i <= 5; i++) {
+  wikis.push({
+    title: faker.lorem.words(),
+    body: faker.lorem.sentence(),
+    private: faker.random.boolean(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    userId: faker.random.number({
+      'min': 25,
+      'max': 26
+    })
+  })
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Wikis", wikis, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Wikis", null, {})
+  }
+};
